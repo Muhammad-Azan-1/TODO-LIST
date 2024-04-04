31430f768ff52db62d3fa72bb6e9e6e2d02f3c8e
Muhammad Azan <155957067+Muhammad-Azan-1@users.noreply.github.com>
2024-04-04 22:35:03 +0500

    Aline the code for asthetic feel

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
-//Initializing a variable with empty Arry 
 let toDos = [];
-while (true) { // using while loop to repeat the code//
+while (true) {
     let answer = await inquirer.prompt([
-        //prompting the user to enter the information A/c to the questions asked//
         {
             message: "What do you want to add in your todos? (please seperate each Item with a comma ',') :",
             name: "todos",
@@ -17,9 +15,7 @@ while (true) { // using while loop to repeat the code//
             default: true,
         },
     ]);
-    // using spilt method to separate every element with comma when writing the answer of the first question in prompt split creates a new arry of those elemenst
     let splitingElements = answer.todos.split(",");
-    // When prompt ask first question, if the answer given by user is empty then execute [empty list] , if the answer contains some thing then execute [answer]
     if (answer.todos !== "") {
         toDos.push(...splitingElements); // ...(spread syntax) coverts each element of split Arry into toDos Arry
         console.log(toDos);
@@ -28,85 +24,82 @@ while (true) { // using while loop to repeat the code//
         toDos.push();
         console.log(toDos);
     }
-    // if the answer provided by the user is no then loop will be breaks
     if (answer.Addmore == false) {
         console.log("\n YOU CAN NOT ENTER ANY THING \n");
         break;
     }
 }
-// working for advance features Remove ,  update or add
+// more features
 while (true) {
     let askingForFeatures = await inquirer.prompt([
         {
-            // asking user that what he wants to do with his current list //
             message: "Do you want to Update , Remove or add any Item in your current list : ",
             type: "confirm",
             name: "string",
         },
     ]);
-    // if in the answer of the question user enter no then loop will breaks
     if (askingForFeatures.string == false) {
         console.log("\nExiting the program\n");
         break;
     }
-    //IN Case if user wants to do some modification
     let modific = await inquirer.prompt([
         {
             message: "Please select any option",
             choices: ["Remove a todo", "update a todo", "Add new todo"],
             type: "list",
             name: "action",
-        }
+        },
     ]);
-    // In case user wants to remove any Item in his todo list 
     if (modific.action === "Remove a todo") {
         let removeTodo = await inquirer.prompt([
             {
-                // asking him to enter the number of the item he wants to delete//
                 message: "Please Enter the  number of the item you want to remove : ",
                 type: "number",
                 name: "NumOfItem",
-            }
+            },
         ]);
-        // checking condition if all the conditions are satisfied then a new toDo list will be print , !isNaN use to ture the Number enter by user , the number user enter should be euqal to 1 or greater then 1 but less then the length of toDo list
-        if (!isNaN(removeTodo.NumOfItem) && removeTodo.NumOfItem >= 1 && removeTodo.NumOfItem <= toDos.length) {
-            let newRemoveTodo = removeTodo.NumOfItem - 1; // minus the number entered by user with 1 to make the index number of that Item 
-            toDos.splice(newRemoveTodo, 1); // splice(startingindex,starting index say jitne numbers delete krne hay,)
-            console.log(toDos);
+        if (!isNaN(removeTodo.NumOfItem) &&
+            removeTodo.NumOfItem >= 1 &&
+            removeTodo.NumOfItem <= toDos.length) {
+            let newRemoveTodo = removeTodo.NumOfItem - 1;
+            toDos.splice(newRemoveTodo, 1);
+        }
+        else {
+            console.log("\nInvaled List number\n");
         }
     }
-    // In case user wants to remove any Item from his todo list 
     if (modific.action === "update a todo") {
         let updateTodo = await inquirer.prompt([
             {
-                message: "\nPlease Enter the number of the item you want to update\n : ",
+                message: "Please Enter the number of the item you want to update : ",
                 type: "number",
                 name: "Num",
             },
-            // now asking the user that what he wants to update instead of the Item whoes number is provided by him
             {
-                message: "\nPLease input the new item\n :",
+                message: "PLease input the new item :",
                 type: "input",
                 name: "Item",
-            }
+            },
         ]);
-        //checking condition if all the conditions are satisfied then a new toDo list will be print , !isNaN use to ture the Number enter by user, the number user enter should be euqal to 1 or greater then 1 but less then the length of toDo list
-        if (!isNaN(updateTodo.Num) && updateTodo.Num >= 1 && updateTodo.Num <= toDos.length) {
-            let newUpdatedTodo = updateTodo.Num - 1; // minus the number entered by user with 1 to make the index number of that Item 
-            toDos.splice(newUpdatedTodo, 1, updateTodo.Item); //// splice(startingindex, starting index say jitne numbers ko delete krna hay, new numbers/string jo add hu gy )
+        if (!isNaN(updateTodo.Num) &&
+            updateTodo.Num >= 1 &&
+            updateTodo.Num <= toDos.length) {
+            let newUpdatedTodo = updateTodo.Num - 1;
+            toDos.splice(newUpdatedTodo, 1, updateTodo.Item);
             console.log(toDos);
         }
+        else if (updateTodo.Num != toDos.length) {
+            console.log("\nInput a valid list number that you wants to update\n");
+        }
     }
-    // if user wants to add a new item in todo list
     if (modific.action === "Add new todo") {
         let addTodo = await inquirer.prompt([
             {
-                message: "Please enter the Todo you want to add ,\n Note:(Please sperate each item with a comma ',') ", //asking user to enter the item he wants to add in todo
+                message: "Please enter the Todo you want to add , Note:(Please sperate each item with a comma ',') ",
                 type: "input",
                 name: "newTodo",
-            }
+            },
         ]);
-        //using isNaN to check if the item is string then the block of code execute but if the item is number then also the same thing happens
         if (isNaN(addTodo.newTodo) || !isNaN(addTodo.newTodo)) {
             let splitingElements = addTodo.newTodo.split(",");
             toDos.push(...splitingElements);
--- main.ts
+++ main.ts
@@ -2,241 +2,131 @@
 
 import inquirer from "inquirer";
 
-//Initializing a variable with empty Arry 
-
 let toDos = [];
-while(true){                                            // using while loop to repeat the code//
-let answer = await inquirer.prompt ([                    
-
-                  //prompting the user to enter the information A/c to the questions asked//
-        {
-            message: "What do you want to add in your todos? (please seperate each Item with a comma ',') :",
-            name: "todos",
-            type: "input", 
-        },  
-        
-         
-        
-        {
-            message:"Do you want to add more things in your Todos? : ",
-            name: "Addmore",
-            type: "confirm",
-           default: true,
-
-        },
-
-        
-
-]);
-
-// using spilt method to separate every element with comma when writing the answer of the first question in prompt split creates a new arry of those elemenst
-
-let splitingElements = answer.todos.split(",")
-
- // When prompt ask first question, if the answer given by user is empty then execute [empty list] , if the answer contains some thing then execute [answer]
-
-if(answer.todos !== "" ){
-
-
-toDos.push(...splitingElements)      // ...(spread syntax) coverts each element of split Arry into toDos Arry
-console.log(toDos)
-
-}else if(answer.todo == null){
-
-    toDos.push()
-    console.log(toDos)
-
-}
-
-
-// if the answer provided by the user is no then loop will be breaks
-
-if(answer.Addmore == false){
-
-   console.log("\n YOU CAN NOT ENTER ANY THING \n")
-    break;
-
-}
-}
-
-
-
-
-// working for advance features Remove ,  update or add
-
-
-while(true){
-let askingForFeatures = await inquirer.prompt([
-
-
+while (true) {
+  let answer = await inquirer.prompt([
     {
-                  // asking user that what he wants to do with his current list //
-
-        message: "Do you want to Update , Remove or add any Item in your current list : ",
-        type: "confirm",
-        name:"string",
-
-
-
+      message:
+        "What do you want to add in your todos? (please seperate each Item with a comma ',') :",
+      name: "todos",
+      type: "input",
     },
 
-
-
-]);
-
-// if in the answer of the question user enter no then loop will breaks
-
-if(askingForFeatures.string == false){
-
-    console.log("\nExiting the program\n")
-    break;
-}
-
-
-//IN Case if user wants to do some modification
-
-let modific = await inquirer.prompt([
-
-
- {
-
-     message:"Please select any option",
-     choices:["Remove a todo","update a todo","Add new todo"],
-     type:"list",
-     name:"action",
-    
-
- }
-
-]);
-
-
-// In case user wants to remove any Item in his todo list 
-
-if(modific.action === "Remove a todo"){
-
-let removeTodo = await inquirer.prompt([
-
     {
-                  // asking him to enter the number of the item he wants to delete//
+      message: "Do you want to add more things in your Todos? : ",
+      name: "Addmore",
+      type: "confirm",
+      default: true,
+    },
+  ]);
 
-        message:"Please Enter the  number of the item you want to remove : ",
-        type:"number",
-        name:"NumOfItem",
-        
+  let splitingElements = answer.todos.split(",");
 
-    }
+  if (answer.todos !== "") {
+    toDos.push(...splitingElements); // ...(spread syntax) coverts each element of split Arry into toDos Arry
+    console.log(toDos);
+  } else if (answer.todo == null) {
+    toDos.push();
+    console.log(toDos);
+  }
 
-
-]);
-
-
-// checking condition if all the conditions are satisfied then a new toDo list will be print , !isNaN use to ture the Number enter by user , the number user enter should be euqal to 1 or greater then 1 but less then the length of toDo list
-
-if(!isNaN(removeTodo.NumOfItem) && removeTodo.NumOfItem >= 1 && removeTodo.NumOfItem  <= toDos.length){
-
-let newRemoveTodo = removeTodo.NumOfItem - 1        // minus the number entered by user with 1 to make the index number of that Item 
-
-toDos.splice(newRemoveTodo,1)    // splice(startingindex,starting index say jitne numbers delete krne hay,)
-console.log(toDos)
-
+  if (answer.Addmore == false) {
+    console.log("\n YOU CAN NOT ENTER ANY THING \n");
+    break;
+  }
 }
 
-}
+// more features
 
+while (true) {
+  let askingForFeatures = await inquirer.prompt([
+    {
+      message:
+        "Do you want to Update , Remove or add any Item in your current list : ",
+      type: "confirm",
+      name: "string",
+    },
+  ]);
 
-// In case user wants to remove any Item from his todo list 
+  if (askingForFeatures.string == false) {
+    console.log("\nExiting the program\n");
+    break;
+  }
 
+  let modific = await inquirer.prompt([
+    {
+      message: "Please select any option",
+      choices: ["Remove a todo", "update a todo", "Add new todo"],
+      type: "list",
+      name: "action",
+    },
+  ]);
 
-if(modific.action === "update a todo"){
-
-
-    let updateTodo = await inquirer.prompt([
-
-        {                                                        // asking user to enter the number of the Item he wants to update //
-    
-            message:"\nPlease Enter the number of the item you want to update\n : ",
-            type:"number",
-            name:"Num",
-    
-        },
-
-                                // now asking the user that what he wants to update instead of the Item whoes number is provided by him
-        {
-            message:"\nPLease input the new item\n :",
-            type:"input",
-            name:"Item",
-
-         }
-    
-    
+  if (modific.action === "Remove a todo") {
+    let removeTodo = await inquirer.prompt([
+      {
+        message: "Please Enter the  number of the item you want to remove : ",
+        type: "number",
+        name: "NumOfItem",
+      },
     ]);
 
-    //checking condition if all the conditions are satisfied then a new toDo list will be print , !isNaN use to ture the Number enter by user, the number user enter should be euqal to 1 or greater then 1 but less then the length of toDo list
+    if (
+      !isNaN(removeTodo.NumOfItem) &&
+      removeTodo.NumOfItem >= 1 &&
+      removeTodo.NumOfItem <= toDos.length
+    ) {
+      let newRemoveTodo = removeTodo.NumOfItem - 1;
 
+      toDos.splice(newRemoveTodo, 1);
+    } else {
+      console.log("\nInvaled List number\n");
+    }
+  }
 
-if(!isNaN( updateTodo.Num) &&  updateTodo.Num >= 1 &&  updateTodo.Num <= toDos.length){
+  if (modific.action === "update a todo") {
+    let updateTodo = await inquirer.prompt([
+      {
+        message: "Please Enter the number of the item you want to update : ",
+        type: "number",
+        name: "Num",
+      },
 
-let newUpdatedTodo = updateTodo.Num - 1; // minus the number entered by user with 1 to make the index number of that Item 
+      {
+        message: "PLease input the new item :",
+        type: "input",
+        name: "Item",
+      },
+    ]);
 
+    if (
+      !isNaN(updateTodo.Num) &&
+      updateTodo.Num >= 1 &&
+      updateTodo.Num <= toDos.length
+    ) {
+      let newUpdatedTodo = updateTodo.Num - 1;
 
-toDos.splice(newUpdatedTodo,1,updateTodo.Item)  //// splice(startingindex, starting index say jitne numbers ko delete krna hay, new numbers/string jo add hu gy )
-console.log(toDos)
- 
- }
- 
+      toDos.splice(newUpdatedTodo, 1, updateTodo.Item);
+      console.log(toDos);
+    } else if (updateTodo.Num != toDos.length) {
+      console.log("\nInput a valid list number that you wants to update\n");
+    }
+  }
 
-
-}
-
-
-// if user wants to add a new item in todo list
-
-
-if(modific.action === "Add new todo"){
-
-
-let addTodo = await inquirer.prompt([
-
-     {
-        message: "Please enter the Todo you want to add ,\n Note:(Please sperate each item with a comma ',') ",    //asking user to enter the item he wants to add in todo
+  if (modific.action === "Add new todo") {
+    let addTodo = await inquirer.prompt([
+      {
+        message:
+          "Please enter the Todo you want to add , Note:(Please sperate each item with a comma ',') ",
         type: "input",
         name: "newTodo",
+      },
+    ]);
 
+    if (isNaN(addTodo.newTodo) || !isNaN(addTodo.newTodo)) {
+      let splitingElements = addTodo.newTodo.split(",");
+      toDos.push(...splitingElements);
+      console.log(toDos);
     }
-
-
-]);
-
-
-//using isNaN to check if the item is string then the block of code execute but if the item is number then also the same thing happens
-
-if(isNaN(addTodo.newTodo) || !isNaN(addTodo.newTodo) ){   
-
-let splitingElements = addTodo.newTodo.split(",")
-toDos.push(...splitingElements)
-console.log(toDos)
-
+  }
 }
-
-
-
-
-}
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
